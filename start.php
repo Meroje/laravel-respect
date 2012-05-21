5eb66a7f0ccef055daee16347bc15c18c6a02861
Jerome Foray <meroje.f@gmail.com>
2012-05-21 22:00:06 +0200

    Updating Components as Submodules

---
+++ start.php
@@ -0,0 +1,6 @@
+<?php
+Autoloader::namespaces(array(
+		'Respect\\Config' => Bundle::path('respect').'config/library/Respect/Config',
+		'Respect\\Relational' => Bundle::path('respect').'relational/library/Respect/Relational',
+		'Respect\\Validation' => Bundle::path('respect').'validation/library/Respect/Validation',
+));
